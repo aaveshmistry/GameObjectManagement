0ebb6a4f79823bd14c494bdb2087ee91a8ae211c
aaveshmistry <48035013+aaveshmistry@users.noreply.github.com>
2019-09-25 15:48:41 +1000

    Initial commit

---
+++ Assets/Scritps/Shape.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : PersistableObject
+{
+    public int MaterialId { get; private set; }
+
+    public void SetMaterial(Material material, int materialId)
+    {
+        GetComponent<MeshRenderer>().material = material;
+        MaterialId = materialId;
+    }
+
+    public int ShapeId
+{
+    get
+    {
+        return ShapeId;
+    }
+
+    set
+    {
+        if (shapeId == int.MinValue && value != int.MinValue)
+        {
+            shapeId = value;
+        }
+        else
+        {
+            Debug.LogError("Not allowed to change shapeId.");
+        }
+     }
+    }
+
+    int shapeId = int.MinValue;
+
+}
